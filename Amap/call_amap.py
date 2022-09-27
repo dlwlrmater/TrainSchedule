ad9124326dc195f3304c6e2524e9517cb06c9c39
dlwlrmater <steven850443850@vip.qq.com>
2022-09-27 20:45:35 +0800

    变更

---
+++ Amap/call_amap.py
@@ -0,0 +1,18 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import pyqtSignal,Qt
+from appm.Amap.amap import Ui_MainWindow_amap
+
+class AmapPageWindow(QWidget,Ui_MainWindow_amap):
+    #声明信号
+    returnSignal = pyqtSignal()
+
+    def __init__(self,parent=None):
+        super(AmapPageWindow, self).__init__(parent)
+        self.setupUi(self)
+        self.initUI()
+
+    def initUI(self):
+        # self.setLayout(self.Form)
+        self.pushButton_amap_backmainpage.clicked.connect(self.returnSignal)
+
